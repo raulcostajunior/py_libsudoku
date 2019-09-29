6d873e33bf6995489c296b946b821bed529517b6
Raul Costa Junior <raulcostajunior@gmail.com>
2019-09-29 17:02:33 +0200

    Added definition stataments for sudoku::Board wrapper (not working yet).

--- src/py_libsudoku.cpp
+++ src/py_libsudoku.cpp
@@ -1,3 +1,10 @@
 #include <pybind11/pybind11.h>
+#include <board.h>
 
 namespace py = pybind11;
+
+PYBIND11_MODULE(py_libsudoku, m) {
+    py::class_<sudoku::Board>(m, "Board")
+        .def(py::init())
+        .def("isValid", &sudoku::Board::isValid);
+}
